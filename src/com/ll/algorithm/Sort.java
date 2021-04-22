b53ecd33e11fef5f27151c2dbef7c8ac6dc8e3ac
‘boylei’ <1119016521@qq.com>
2021-04-22 20:48:31 +0800

    排序

---
+++ src/com/ll/algorithm/Sort.java
@@ -0,0 +1,100 @@
+package com.ll.algorithm;
+
+/**
+ * 主要记录一下十个排序算法的实现过程,实现的都是从小到大的排序
+ * @author Lei
+ * @version 0.1
+ * @date 2021/4/22
+ */
+public class Sort {
+
+    /**
+     *
+     * @param a 数组a
+     * @param i 位置i
+     * @param j 位置j
+     */
+    public static void swap(int[] a, int i,int j){
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    /**
+     * 冒泡排序
+     * @param a 数组a
+     */
+    public static void bubbleSort(int[] a){
+        int length = a.length;
+        for (int i = 0; i < length-1; i++){
+            for (int j = i+1; j<length;j++){
+                if (a[i] >= a[j]){
+                    swap(a, i, j);
+                }
+            }
+        }
+    }
+
+    /**
+     * 选择排序
+     * @param a 数组a
+     */
+    public static void selectionSort(int[] a){
+        int length = a.length;
+        for (int i = 0; i < length-1; i++) {
+            int minindex = i;
+            for (int j = i+1; j < length; j++) {
+                if (a[j] < a[minindex]){
+                    minindex = j;
+                }
+            }
+            swap(a, i, minindex);
+        }
+    }
+
+    /**
+     * 插入排序
+     * @param a 数组a
+     */
+    public static void insertiongSort(int[] a){
+        int length = a.length;
+        for (int i = 1; i < length; i++) {
+            int index = i-1;
+            int current = a[i];
+            while (index >=0 && a[index] > current){
+                a[index+1] = a[index];
+                index--;
+            }
+            a[index+1]=current;
+        }
+    }
+
+
+    /**
+     * 希尔排序
+     * @param a
+     */
+    public static void shellSort(int[] a){
+
+    }
+
+
+    /**
+     * 归并排序
+     * @param a
+     */
+    public static void mergeSort(int[] a){
+        
+    }
+
+
+    public static void main(String[] args) {
+        int[] a = new int[]{1,4,3,76,3,2,6,11};
+        insertiongSort(a);
+        for (Integer integer : a){
+            System.out.println(integer);
+        }
+    }
+
+
+}
