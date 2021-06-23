0333188af5802fdcde8ee87979d1df3c62df710c
liulei <1119016521@qq.com>
2021-06-23 22:08:03 +0800

    zsh 更新

--- scripts/zsh/zsh_dow.sh
+++ scripts/zsh/zsh_dow.sh
@@ -3,6 +3,3 @@
 apt install zsh
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-#echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >>~/.zprofile
-#eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
