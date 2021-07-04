e46966b1d6aa123d3b0dc0f96657f9ed9798b56b
liulei <1119016521@qq.com>
2021-07-04 10:32:49 +0800

    k8s安装

--- scripts/k8s/k8s_install.sh
+++ scripts/k8s/k8s_install.sh
@@ -1,24 +1,33 @@
 #!/bin/bash
 
 
-K8S_VERSION=1.18.6
+K8S_VERSION=1.19.8
 if [[ $# == 1 ]]; then
     echo "$1 k8s_version"
     K8S_VERSION=$1
 fi
 
+swapoff -a && sysctl -w vm.swappiness=0  # 关闭swap
+
+sudo sed -ri '/^[^#]*swap/s@^@#@' /etc/fstab  # 取消开机挂载swap
+
+systemctl stop ufw
+systemctl disable ufw
+# ufw allow 22
+
 curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | sudo apt-key add -
-cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
-deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-$(lsb_release -cs) main
+# deb [arch=arm64] https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
+deb [arch=amd64] https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
 EOF
 apt update
 
-#apt install -y kubelet=1.18.6-00 kubeadm=1.18.6-00 kubectl=1.18.6-00
 apt install -y kubelet=$K8S_VERSION-00 kubeadm=$K8S_VERSION-00 kubectl=$K8S_VERSION-00
 
 # ipv4 转发
 sed -i 's/#net.ipv4.ip_forward=1/net.ipv4.ip_forward=1/g' /etc/sysctl.conf
-sudo sysctl -p
+sysctl -p
 
 #
 cat <<EOF > /etc/sysctl.d/k8s.conf
